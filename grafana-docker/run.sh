de2476489fd4a51cfcc1e23f0459ab6ff9a7c249
Artemiy <artemiy.vyacheslavov@gmail.com>
2018-02-19 04:09:29 +0100

    Update run.sh

--- grafana-docker/run.sh
+++ grafana-docker/run.sh
@@ -3,7 +3,8 @@
 : "${GF_PATHS_CONFIG:=/etc/grafana/grafana.ini}"
 : "${GF_PATHS_DATA:=/var/lib/grafana}"
 : "${GF_PATHS_LOGS:=/var/log/grafana}"
-: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+#: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+: "${GF_PATHS_PLUGINS=/data/grafana/plugins}"
 : "${GF_PATHS_PROVISIONING:=/etc/grafana/provisioning}"
 
 chown -R grafana:grafana "$GF_PATHS_DATA" "$GF_PATHS_LOGS"
