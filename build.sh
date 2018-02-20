e71b50262f844d4f2ca304f7bcf259e85b83c062
Artemiy <artemiy.vyacheslavov@gmail.com>
2018-02-20 14:08:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-_grafana_tag=$1
-_grafana_version=${_grafana_tag:1}
+_grafana_plugins=$1
+_grafana_tag=$2
+_grafana_version=$3
 
-if [ "$_grafana_version" != "" ]; then
-	echo "Building version ${_grafana_version}"
-	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"
+
+
+if [ ${_grafana_tag} == "" ]; then
+	_grafana_tag="master"
+fi
+
+if [ ${_grafana_version} == "" ]; then
+	_grafana_version="latest"
+fi
+
+
+echo "Building Docker image for Grafana of ${_grafana_version} version with a tag ${_grafana_tag}"
+echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"	
+
+if [ "$_grafana_plugins" != "" ]; then
+	echo "List of plugins: ${_grafana_plugins}"
 	docker build \
-		--build-arg GRAFANA_VERSION=latest \
-		--tag "grafana/grafana:${_grafana_version}" \
+		--build-arg GRAFANA_VERSION=${_grafana_version} GF_INSTALL_PLUGINS=${_grafana_plugins} \
+		--tag "grafana/grafana:${_grafana_tag}" \
 		--no-cache=true .
-	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
+#	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
 
 else
-	echo "Building latest for master"
+	echo "No plugins will be installed"
 	docker build \
-		--tag "grafana/grafana:master" \
+		--build-arg GRAFANA_VERSION=${_grafana_version} \
+		--tag "grafana/grafana:${_grafana_tag}" \
 		--no-cache=true .
 fi
