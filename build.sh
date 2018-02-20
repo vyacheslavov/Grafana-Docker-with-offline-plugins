5a78e9e0409e02e6d7a7dafca07a0224bb4e2747
Artemiy <artemiy.vyacheslavov@gmail.com>
2018-02-20 14:47:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,18 +22,17 @@ fi
 echo "Building Docker image for Grafana of ${_grafana_version} version with a tag ${_grafana_tag}"
 echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"	
 
-if [ "$_grafana_plugins" != "" ]; then
+if [ -z "$1" ]; then
+	echo "No plugins will be installed"
+	docker build \
+		--build-arg GRAFANA_VERSION=${_grafana_version} \
+		--tag "grafana/grafana:${_grafana_tag}" \
+		--no-cache=true .
+else
 	echo "List of plugins: ${_grafana_plugins}"
 	docker build \
 		--build-arg GRAFANA_VERSION=${_grafana_version} GF_INSTALL_PLUGINS=${_grafana_plugins} \
 		--tag "grafana/grafana:${_grafana_tag}" \
 		--no-cache=true .
 #	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
-
-else
-	echo "No plugins will be installed"
-	docker build \
-		--build-arg GRAFANA_VERSION=${_grafana_version} \
-		--tag "grafana/grafana:${_grafana_tag}" \
-		--no-cache=true .
 fi
